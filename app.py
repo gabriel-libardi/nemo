c40b11dd683fb30fd1f7df79c2a9995557ef4de1
gabriel-libardi <libardi.120205@protonmail.com>
2024-02-05 12:02:03 -0300

    Corrected static files paths. Added waitress

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from waitress import serve 
 
 app = Flask(__name__)
 
@@ -8,4 +9,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    # app.run(host='0.0.0.0')
+    serve(app, host='0.0.0.0', port=8080, url_scheme='https')
+
